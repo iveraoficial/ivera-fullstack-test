56441f172a341fdc09bff62b084e021f6da3d205
Daniel Inácio <danielhi2013@hotmail.com>
2022-11-10 19:32:19 -0300

    * criacao das estruturas de pastas * criacao de funcao para retornar hash da api da marvel

---
+++ api/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import router from "./src/config/routes.js";
+import {montarQueryStringApi} from "./src/app/Helpers/queryStringApi.js";
+
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+montarQueryStringApi();
+
+app.use(router)
+
+const port = process.env.APP_PORT;
+app.listen(port, () => {console.log(`A aplicacao esta rodando na porta ${port}...`)});
