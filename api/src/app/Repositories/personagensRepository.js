c4fb21085b5e2ce8d190f22f2aeb20d03154be34
Daniel Inácio <danielhi2013@hotmail.com>
2022-11-15 14:04:33 -0300

    Pegando personagem da api por id

--- api/src/app/Repositories/personagensRepository.js
+++ api/src/app/Repositories/personagensRepository.js
@@ -8,12 +8,20 @@ const listarPersonagens = (offset, limite, name) => {
     if(name !== null) {
         url += `&nameStartsWith=${name}`
     }
+    // console.log("URL", url)
+    return axios.get(url);
+}
+
+const pegarPersonagemPorId = (id) => {
+    const apiAuth = montarQueryStringApi();
+    let url = `/v1/public/characters/${id}?${apiAuth}`;
+
     console.log("URL", url)
     return axios.get(url);
 }
 
-
 // let url = `/v1/public/characters/${id}?${apiAuth}
 export {
-    listarPersonagens
+    listarPersonagens,
+    pegarPersonagemPorId
 }
